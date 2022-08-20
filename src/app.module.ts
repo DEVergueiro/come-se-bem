1e4c9c8974b1acb579fb9d6a12f38765987cf448
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-19 21:57:39 -0300

    renomeando arquivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ModuleModule } from './pedido/module/module.module';
 import { PedidoModule } from './pedido/order.module';
+import { ProductModule } from './product/product.module';
 import { ProdutoModule } from './produto/produto.module';
 import { PedidoModule } from './pedido/order.module';
 
 @Module({
-  imports: [ModuleModule, PedidoModule, ProdutoModule],
+  imports: [ModuleModule, PedidoModule, ProdutoModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,13 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaProvider } from './providers/prisma.provider';
-import { ContentsRepository } from './repositories/produtorepository';
-import { PlaylistsRepository } from './repositories/playlists.repository';
+import { ProductRepository } from './repositories/product.repository';
+import { OrderRepository } from './repositories/order.repository';
 
 // Esse decorator esta sendo usado para que tudo que ta
 // aqui dentro possa ser usado em qualquer lugar
 @Global()
 @Module({
-  providers: [PrismaProvider, PlaylistsRepository, ContentsRepository],
+  providers: [PrismaProvider, OrderRepository, ProductRepository],
   exports: [PrismaProvider],
 })
 export class DatabaseModule {}
