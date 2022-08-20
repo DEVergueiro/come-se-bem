e0f9599efce7abcc2395253f2e75968051ece71e
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-19 22:22:42 -0300

    corrigindo nomenclaturas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ModuleModule } from './pedido/module/module.module';
-import { PedidoModule } from './pedido/order.module';
+import { OrderModule } from './pedido/order.module';
 import { ProductModule } from './product/product.module';
-import { ProdutoModule } from './produto/produto.module';
-import { PedidoModule } from './pedido/order.module';
 
 @Module({
-  imports: [ModuleModule, PedidoModule, ProdutoModule, ProductModule],
+  imports: [OrderModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/infrastructure/http/controllers/product.controller.ts
+++ src/infrastructure/http/controllers/product.controller.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@nestjs/common';
-import { ProductService } from './product.service';
+import { ProductService } from '../../../product/product.service';
 
 @Controller('product')
 export class ProductController {
