ea7076f98f41c9a12c2c36991de0ef401d3305b8
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-19 21:21:04 -0300

    add swagger

---
+++ src/infrastructure/configs/swagger.config.ts
@@ -0,0 +1,10 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerConfig = new DocumentBuilder()
+  .setTitle('Come-se Bem Service API.')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .setDescription(
+    'This is the documentation of the API consumed by the Come-se Bem frontend app',
+  )
+  .build();
