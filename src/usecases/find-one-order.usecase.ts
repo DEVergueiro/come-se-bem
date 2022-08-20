bffc5e7eb165ea58d42b223ede618f5b2bef3145
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-19 22:22:59 -0300

    criando usecases

---
+++ src/usecases/find-one-order.usecase.ts
@@ -0,0 +1,9 @@
+import { OrderRepository } from 'src/infrastructure/database/repositories/order.repository';
+
+export class FindOneOrderUsecase {
+  constructor(private readonly orderRepository: OrderRepository) {}
+
+  async execute(id: string) {
+    return this.orderRepository.findOne(id);
+  }
+}
