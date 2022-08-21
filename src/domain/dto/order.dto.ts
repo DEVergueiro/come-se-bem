cb9388b98e0d930c2377550cc8117c55682e2051
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-21 15:45:29 -0300

    atualizando dto

--- src/domain/dto/order.dto.ts
+++ src/domain/dto/order.dto.ts
@@ -2,13 +2,21 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class OrderDTO {
   @ApiProperty()
-  id?: number;
+  id?: any;
   @ApiProperty()
   client: string;
   @ApiProperty()
   amount: number;
   @ApiProperty()
+  amount_paid: number;
+  @ApiProperty()
+  change: number;
+  @ApiProperty()
   delivered: boolean;
+  @ApiProperty()
+  observation: string;
+  @ApiProperty()
+  payment: any;
   @ApiProperty()
   createdAt: string;
   @ApiProperty()
--- src/domain/dto/product.dto.ts
+++ src/domain/dto/product.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ProductDTO {
   @ApiProperty()
-  id?: number;
+  id?: any;
   @ApiProperty()
   name: string;
   @ApiProperty()
