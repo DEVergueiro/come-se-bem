762fe7370f60d80d280b2919df7ad4eaa579d898
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-21 15:44:28 -0300

    ajustando arquitetura

--- src/pedido/order.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrderService } from './order.service';
-import { OrderController } from '../infrastructure/http/controllers/order.controller';
-
-@Module({
-  controllers: [OrderController],
-  providers: [OrderService]
-})
-export class OrderModule {}
--- src/product/product.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductService } from './product.service';
-import { ProductController } from '../infrastructure/http/controllers/product.controller';
-
-@Module({
-  controllers: [ProductController],
-  providers: [ProductService]
-})
-export class ProductModule {}
