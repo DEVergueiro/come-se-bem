ee7dab555964d444e9f51c4eb756d5d37dd28a9a
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-22 16:46:25 -0300

    feat: rename files

--- src/usecases/find-one-order.usecase.ts
+++
@@ -1,9 +0,0 @@
-import { OrderRepository } from 'src/infrastructure/database/repositories/order.repository';
-
-export class FindOneOrderUsecase {
-  constructor(private readonly orderRepository: OrderRepository) {}
-
-  async execute(id: string) {
-    return this.orderRepository.findOne(id);
-  }
-}
--- src/usecases/find-one-product.usecase.ts
+++
@@ -1,9 +0,0 @@
-import { ProductRepository } from 'src/infrastructure/database/repositories/product.repository';
-
-export class FindOneProductUsecase {
-  constructor(private readonly productRepository: ProductRepository) {}
-
-  async execute(id: string) {
-    return this.productRepository.findOne(id);
-  }
-}
