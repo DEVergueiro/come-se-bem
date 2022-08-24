025b735d4ff90fad77ce0baf54250192fc123a9b
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-24 20:58:52 -0300

    chore: create find all orders

--- src/infrastructure/http/controllers/order-controller.ts
+++ src/infrastructure/http/controllers/order-controller.ts
@@ -4,6 +4,7 @@ import { OrderRepository } from 'src/infrastructure/database/repositories/order-
 
 import { OrderDTO } from '../../../domain/dto/order-dto';
 import { CreateOrderUsecase } from '../../../usecases/create-order-usecase';
+import { FindAllOrderUsecase } from '../../../usecases/find-all-order-usecase';
 import { FindOneOrderUsecase } from '../../../usecases/find-one-order-usecase';
 import { UpdateOrderUsecase } from '../../../usecases/update-order-usecase';
 
@@ -14,12 +15,15 @@ export class OrderController {
 
   private readonly findOneOrderUsecase: FindOneOrderUsecase;
 
+  private readonly findAllOrderUsecase: FindAllOrderUsecase;
+
   private readonly updateOrderUsecase: UpdateOrderUsecase;
 
   constructor(private readonly orderRepository: OrderRepository) {
     this.createOrderUsecase = new CreateOrderUsecase(this.orderRepository);
     this.findOneOrderUsecase = new FindOneOrderUsecase(this.orderRepository);
     this.updateOrderUsecase = new UpdateOrderUsecase(this.orderRepository);
+    this.findAllOrderUsecase = new FindAllOrderUsecase(this.orderRepository);
   }
 
   @Post()
@@ -30,6 +34,11 @@ export class OrderController {
   @Get('/:id')
   async findOne(@Param('id') id: string) {
     return this.findOneOrderUsecase.execute(id);
+  }
+
+  @Get()
+  async findAll() {
+    return this.findAllOrderUsecase.execute();
   }
 
   @Put('/:id')
