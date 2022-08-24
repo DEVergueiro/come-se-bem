39673e724055bf7be92fe3b1d76c5348958af17e
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-24 12:29:24 -0300

    feat: create print/update order

--- src/infrastructure/database/repositories/order-repository.ts
+++ src/infrastructure/database/repositories/order-repository.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { PrinterTypes, ThermalPrinter } from 'node-thermal-printer/node-thermal-printer';
 import { PrismaProvider } from 'src/infrastructure/database/providers/prisma-provider';
 
 import { OrderDTO } from '../../../domain/dto/order-dto';
@@ -21,5 +22,49 @@ export class OrderRepository {
     });
 
     return order;
+  }
+
+  async update(id: string, data: OrderDTO) {
+    const orderExists = await this.prisma.order.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!orderExists) {
+      throw new Error('Order does not exists!');
+    }
+
+    return this.prisma.order.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  async printOrder(id: string) {
+    const order = await this.prisma.order.findFirst({
+      where: {
+        id,
+      },
+    });
+    return order;
+
+    const printer = new ThermalPrinter({
+      type: PrinterTypes.EPSON,
+      interface: 'tcp://xxx.xxx.xxx.xxx',
+    });
+    printer.alignLeft();
+    printer.setPrinterDriver(order);
+    printer.cut();
+  }
+
+  async findDelivered() {
+    return this.prisma.order.findMany({
+      where: {
+        delivered: true,
+      },
+    });
   }
 }
