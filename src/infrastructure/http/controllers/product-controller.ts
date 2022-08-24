1394cbe296e235fd9a301d576411faa0213574dc
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-24 12:28:36 -0300

    chore: add tag swagger

--- src/infrastructure/http/controllers/product-controller.ts
+++ src/infrastructure/http/controllers/product-controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ProductRepository } from 'src/infrastructure/database/repositories/product-repository';
 
 import { FindIdProductUsecase } from '../../../usecases/find-id-product-usecase';
 import { FindNameProductUsecase } from '../../../usecases/find-name-product-usecase';
 
+@ApiTags('product')
 @Controller('product')
 export class ProductController {
   private readonly findIdProductUsecase: FindIdProductUsecase;
