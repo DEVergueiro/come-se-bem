5e2efd8e87749311012e5b2702feeda2c4e6e615
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-08-25 12:34:01 -0300

    chore: adjust exports

--- src/infrastructure/database/database-module.ts
+++ src/infrastructure/database/database-module.ts
@@ -4,11 +4,9 @@ import { PrismaProvider } from './providers/prisma-provider';
 import { OrderRepository } from './repositories/order-repository';
 import { ProductRepository } from './repositories/product-repository';
 
-// Esse decorator esta sendo usado para que tudo que ta
-// aqui dentro possa ser usado em qualquer lugar
 @Global()
 @Module({
   providers: [PrismaProvider, OrderRepository, ProductRepository],
-  exports: [PrismaProvider],
+  exports: [PrismaProvider, OrderRepository, ProductRepository],
 })
 export class DatabaseModule {}
